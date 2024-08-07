68465690c63db4a5536e1dab99f465de47310580
LucyDillon <62658632+LucyDillon@users.noreply.github.com>
2024-08-07 11:40:36 +0100

    Create sourmash_ANI_csv.sh

---
+++ sourmash_ANI_csv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --time=23:59:59
+#SBATCH --partition=k2-medpri,bio-compute
+#SBATCH --mem=100G
+#SBATCH --job-name=sourmash
+
+source activate /mnt/scratch2/users/40309916/smash
+
+sourmash plot ani.cmp --csv plot_ANI_output.csv
